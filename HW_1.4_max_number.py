1201319d25002aea76a2e572cb543df52f3ac77f
kcflint <kcflint59@gmail.com>
2020-12-03 16:38:53 +0300

    hw1 is done

---
+++ HW_1.4_max_number.py
@@ -0,0 +1,13 @@
+number = 0
+while number <= 0:
+    number = int(input('Введите целое положительное число: '))
+
+max_number = 0
+number_n = number
+
+for i in range(len(str(number))):
+    digit = number_n // 10**(len(str(number))-i)
+    if digit > max_number:
+        max_number = digit
+    number_n = number_n - (digit * 10**(len(str(number))-i))
+print(f'Самая большая цифра в числе {number} - {max_number}')
