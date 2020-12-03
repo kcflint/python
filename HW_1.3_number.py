878f06d5d3edf3b85721c8d727b67c3d9a11bbd9
kcflint <kcflint59@gmail.com>
2020-12-03 17:05:59 +0300

    lesson1

--- HW_1.3_number.py
+++
@@ -1,6 +0,0 @@
-number1 = int(input('Введите цифру от 1 до 9: '))
-number2 = str(number1)*2
-number3 = str(number1)*3
-total = number1 + int(number2) + int(number3)
-
-print(f'{number1}+{number2}+{number3}={total}')
